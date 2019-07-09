3716b20e5d378e8abf712b9b965628588129685f
Ale Muñoz <bomberstudios@gmail.com>
2019-07-09 18:12:08 +0200

    Use a real action as an example

--- template/src/my-command.js
+++ template/src/my-command.js
@@ -5,8 +5,10 @@ export default function() {
   sketch.UI.message("It's alive 🙌")
 }
 
-export function ActionHandler(context){
+export function onOpenDocument(context){
   console.log(`Action name: ${context.actionContext.name}`)
   console.log(`Action: ${context.actionContext}`)
   // Do something here
+  // For a general overview of the Actions API, check https://developer.sketch.com/plugins/actions
+  // For a complete list of other Actions, check https://developer.sketch.com/reference/action/
 }
